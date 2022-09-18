82b3ba220430af0579a07e5f1f9b06a6c0484bbc
Keshaw Ram <keshaw.ram@allegion.com>
2022-09-18 10:56:51 +0530

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Ram, Keshaw on 18/09/22.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
