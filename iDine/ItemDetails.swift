18f6648d94adf0587efd88114f4a5491f7209517
kram <kram@allegion.com>
2024-12-29 19:12:53 +0530

    initial commit

---
+++ iDine/ItemDetails.swift
@@ -0,0 +1,20 @@
+//
+//  itemDetails.swift
+//  iDine
+//
+//  Created by Ram, Keshaw on 19/09/22.
+//
+
+import SwiftUI
+
+struct itemDetails: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct itemDetails_Previews: PreviewProvider {
+    static var previews: some View {
+        itemDetails()
+    }
+}
---
+++ iDine/ItemRow.swift
@@ -0,0 +1,20 @@
+//
+//  itemRow.swift
+//  iDine
+//
+//  Created by Ram, Keshaw on 18/09/22.
+//
+
+import SwiftUI
+
+struct itemRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct itemRow_Previews: PreviewProvider {
+    static var previews: some View {
+        itemRow()
+    }
+}
